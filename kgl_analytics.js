5598fd5469fbcb3f5961f69364477b0f4e68e928
greatkevingkpro-svg <greatkevingkpro@gmail.com>
2025-12-30 12:46:46 +0300

    js coursework3

---
+++ kgl_analytics.js
@@ -0,0 +1,47 @@
+// Coursework 3: KGL Analytics and Reporting System
+
+
+// Part A: Higher Order Functions - Data Transformation
+
+let procurementRecords = [
+  {
+    id: 1001,
+    dealerName: "Frank",
+    produceType: "Maize",
+    tonnageInKgs: 1200,
+    costInUgx: 2400000,
+    procurementDate: new Date("2025-01-05")
+  },
+  {
+    id: 1002,
+    dealerName: "Sarah",
+    produceType: "Beans",
+    tonnageInKgs: 800,
+    costInUgx: 1600000,
+    procurementDate: new Date("2025-01-05")
+  },
+  {
+    id: 1003,
+    dealerName: "David",
+    produceType: "G-nuts",
+    tonnageInKgs: 2000,
+    costInUgx: 6000000,
+    procurementDate: new Date("2025-01-09")
+  },
+  {
+    id: 1004,
+    dealerName: "Alice",
+    produceType: "Sorghum",
+    tonnageInKgs: 1100,
+    costInUgx: 3300000,
+    procurementDate: new Date("2025-01-10")
+  },
+  {
+    id: 1005,
+    dealerName: "Anna",
+    produceType: "Rice",
+    tonnageInKgs: 1500,
+    costInUgx: 7500000,
+    procurementDate: new Date("2025-01-25")
+  }
+];
